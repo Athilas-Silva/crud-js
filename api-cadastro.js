2047e740caa38a10370c584f7e1765eb6d3c9c87
Lucas Galdino <71888039+LucasGaldinno@users.noreply.github.com>
2021-09-02 14:13:38 -0300

    Add files via upload

--- api-cadastro.js
+++ api-cadastro.js
@@ -4,33 +4,7 @@ const app = express();
 app.use(cors());
 const mysql = require ("mysql");
 const bodyParser = require("body-parser");
-const port = 8080; 
-const multer = require('multer');
-let data;
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-       cb(null, './uploads/');
-  },
-  filename: function(req, file, cb) {
-    data = new Date().toISOString().replace(/:/g, '-') + '-';
-     cb(null, data + file.originalname);  
-        
-    }
-});
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-}
-const upload = multer({ 
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-    fileFilter: fileFilter
-});
+const port = 8080;
 
 // Configurando body-parser para pegar os POSTS mais tarde
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -78,16 +52,14 @@ router.delete('/loginuser/:idUser', (req, res) =>{
   execSQLQuery('DELETE FROM loginuser WHERE idUser=' + parseInt(req.params.idUser), res);
 });
 
-router.post('/loginuser', upload.single('imagem_user'), (req, res) => {
-console.log(req.file);
+router.post('/loginuser', (req, res) => {
   const emailUser = req.body.emailUser.substring(0,100);
   const nome = req.body.nome.substring(0,100);
   const cargo = req.body.cargo.substring(0,45);
   const salario = req.body.salario.substring(0,50);
   const setor = req.body.setor.substring(0,45);
-  const senhaUser = req.body.senhaUser.substring(0,45); 
-  const imagem = 'uploads/' + req.file.filename;  
-  execSQLQuery(`INSERT INTO loginuser(emailUser, nome, cargo, salario, setor, senhaUser, imagem_user) VALUES('${emailUser}', '${nome}', '${cargo}', '${salario}', '${setor}', '${senhaUser}', '${imagem}')`, res);
+  const senhaUser = req.body.senhaUser.substring(0,45);  
+  execSQLQuery(`INSERT INTO loginuser(emailUser, nome, cargo, salario, setor, senhaUser) VALUES('${emailUser}', '${nome}', '${cargo}', '${salario}', '${setor}', '${senhaUser}')`, res);
 });
 
 router.patch('/loginuser/:idUser', (req, res) => {
---
+++ new-senha.js
@@ -0,0 +1,5 @@
+function newSenha() {
+  let email = prompt("Digite seu Email Aqui:");
+  console.log(email);
+  window.location.reload();
+}
